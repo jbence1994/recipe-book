ae94c8db4839a73157d7e171639bf67db42c97c0
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-14 10:18:59 +0200

    Initial commit

---
+++ src/main/java/com/github/jbence1994/recipebook/RecipeBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.recipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jbence1994/recipebook/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.recipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
